a44c19be73177059293185a4af0c004e45a05372
amanecer <amanecer.luz.cabrera@gmail.com>
2023-06-02 01:21:15 -0400

    added exercises react

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+export const GifExpertApp = () => {  
+    return(
+        <>
+        {/*titulo */}
+            <h1>GifExpertApp</h1>
+        {/*input*/}
+
+        {/*listado de gif */}
+        {/*GiffItem */}
+
+        </>
+
+    )
+}
